e9e0ff3bc01e9cdd062b2c0bbd98dca237e36b0b
tolanny-studio <hellotolannystudio@gmail.com>
2026-07-08 18:30:43 +0100

    decomposed code to get_boundaries and bisection_guess function

--- bisection-check/main.py
+++ bisection-check/main.py
@@ -1,20 +1,30 @@
 import random
 
-lower_boundary = int(input("Enter the lower boundary : "))
-upper_boundary = int(input("Enter the upper boundary : "))
+def get_boundaries():
+    lower_boundary = int(input("Enter the lower boundary : "))
+    upper_boundary = int(input("Enter the upper boundary : "))
+    return lower_boundary, upper_boundary
 
 
-secret_number = random.randint(lower_boundary, upper_boundary)
+def bisection_guess():
+    lower_boundary, upper_boundary = get_boundaries()
+    secret_number = random.randint(lower_boundary, upper_boundary)
 
-print(secret_number)
-tries = 0
-while True:
-    mid_section = int((upper_boundary + lower_boundary) // 2)
-    tries += 1
-    if secret_number == mid_section:
-        print(f"You got it 💯 => Tries : {tries}")
-        break
-    if secret_number > mid_section:
-        lower_boundary = mid_section
-    else:
-        upper_boundary = mid_section
+    print(secret_number)
+    tries = 0
+    while True:
+        mid_section = int((upper_boundary + lower_boundary) // 2)
+        tries += 1
+        if secret_number == mid_section:
+            print(f"You got it 💯 => Tries : {tries}")
+            break
+        if secret_number > mid_section:
+            lower_boundary = mid_section
+        else:
+            upper_boundary = mid_section
+
+def main():
+  bisection_guess()
+  
+if __name__ == "__main__":
+  main()
